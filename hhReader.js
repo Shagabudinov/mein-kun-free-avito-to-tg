7b980d41063b9e14f048e44ad41709fd19d4f760
Shagabudinov <cherrygumfd@mail.ru>
2025-08-05 22:37:40 +0300

    first commit

---
+++ hhReader.js
@@ -0,0 +1,110 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { supabase } from './supabaseInit.js';
+
+const keyWords = ['frontend', 'фронтенд', 'react']
+
+dotenv.config();
+
+let pageNumber = 1;
+const url = process.env.HH_SEARCH_URL;
+
+const filterItems = (items) => {
+  return items.filter((item) => {
+    const title = item.name.toLowerCase();
+    return (
+      title.includes(keyWords[0]) ||
+      title.includes(keyWords[1]) ||
+      title.includes(keyWords[2])
+    );
+
+  });
+};
+
+async function addToSupaBase(items) {
+  const itemsToTable = items.map((item) => {
+    const from = item?.compensation?.from;
+    const to = item?.compensation?.to;
+    const currency = item?.compensation?.currencyCode?.replace('RUR', 'RUB');
+    const money = !from ?? !to
+      ? 'Зп не указана'
+      : from && !to
+      ? `От ${from} ${currency}`
+      : !from && to
+      ? `До ${from} ${currency}`
+      : `${from}-${to} ${currency}`;
+    return {
+      id: item.vacancyId,
+      title: item.name,
+      money: money,
+      sended_to_telegram: false,
+    };
+  });
+
+  if (itemsToTable.length > 0) {
+    try {
+      // Проверяем наличие записей с таким id и sended_to_telegram == true
+      const existingItems = await supabase
+        .from('hh-kaluga')
+        .select('id, sended_to_telegram')
+        .in(
+          'id',
+          itemsToTable.map((item) => item.id)
+        )
+        .eq('sended_to_telegram', true);
+
+      // Фильтруем те элементы, которые уже имеют sended_to_telegram == true
+      const filteredItems = itemsToTable.filter((item) => {
+        return !existingItems.data.some(
+          (existingItem) => existingItem.id === item.id
+        );
+      });
+
+      if (filteredItems.length > 0) {
+        const { data, error } = await supabase
+          .from('hh-kaluga')
+          .upsert(filteredItems, { onConflict: 'id' });
+
+        if (error) {
+          console.error('Ошибка при добавлении в базу данных:', error);
+        } else {
+          console.log('Успешно добавлено в базу данных:', data);
+        }
+      } else {
+        console.log(
+          'Все записи с такими id уже имеют sended_to_telegram == true. Пропускаем.'
+        );
+      }
+    } catch (error) {
+      console.error('Ошибка при запросе в базу данных:', error);
+    }
+  } else {
+    console.log('Нет данных для добавления.');
+    pageNumber = 1;
+  }
+}
+
+const getDataFromHH = () => {
+  setInterval(async () => {
+    try {
+      //console.log(`${url}&page=${pageNumber}`);
+      const response = await axios.get(`${url}`);
+      pageNumber += 1;
+      const items = response.data.vacancySearchResult.vacancies;
+      console.log(`Вакансий всего: ${items.length}`);
+
+      // Фильтруем данные
+      //const filteredItems = filterItems(items);
+      console.log(`Вакансий для Калуги: ${items.length}`);
+
+      // Добавляем отфильтрованные данные в базу
+      await addToSupaBase(items);
+
+      //console.log(`Обработано ${filteredItems.length} элементов.`);
+    } catch (error) {
+      console.error('Ошибка при получении данных с Avito:', error);
+    }
+  }, 6000);
+};
+
+getDataFromHH();
---
+++ telegram-bot.py
@@ -0,0 +1,83 @@
+import asyncio
+import os
+import time
+from telegram import Bot
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+bot = Bot(token=TELEGRAM_TOKEN)
+
+last_sent_time = 0
+message_interval = 5  # секунд между сообщениями
+
+async def send_start_message():
+    try:
+        await bot.send_message(chat_id=CHAT_ID, text="Бот запущен")
+    except Exception as e:
+        print("Ошибка отправки стартового сообщения:", e)
+
+async def send_shutdown_message():
+    try:
+        await bot.send_message(chat_id=CHAT_ID, text="Бот отключен пользователем")
+    except Exception as e:
+        print("Ошибка отправки сообщения об отключении:", e)
+
+async def check_and_send():
+    global last_sent_time
+
+    while True:
+        current_time = time.time()
+
+        if current_time - last_sent_time >= message_interval:
+            response = supabase.table("hh-kaluga").select("id, title, money").eq("sended_to_telegram", False).execute()
+
+            print(f'Response: {response.data} count={response.count}')
+
+            for record in response.data:
+                id = record['id']
+                title = record['title']
+                money = record['money']
+
+                message = (
+                    f"<b>{title}</b>\n"
+                    f"Оплата: {money}\n"
+                    f"<a href=\"https://hh.ru/vacancy/{id}\">Перейти на HH</a>"
+                )
+
+                # Обработка ошибок при отправке
+                try:
+                    await bot.send_message(
+                        chat_id=CHAT_ID,
+                        text=message,
+                        parse_mode='HTML',
+                        disable_web_page_preview=True
+                    )
+                    print(f"Отправлено: {title}")
+
+                    supabase.table("hh-kaluga").update({"sended_to_telegram": True}).eq("id", id).execute()
+                except Exception as e:
+                    print(f"Ошибка при отправке вакансии '{title}':", e)
+                    await asyncio.sleep(15)
+
+                last_sent_time = current_time
+                break
+
+        await asyncio.sleep(10)
+
+async def main():
+    try:
+        await send_start_message()
+        await check_and_send()
+    finally:
+        await send_shutdown_message()
+
+if __name__ == "__main__":
+    asyncio.run(main())
